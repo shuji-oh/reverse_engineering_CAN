2178fa6efe295fcb8b8a162aed081ac255651efd
ohirangosta <syuji0116@gmail.com>
2018-09-27 10:50:13 +0900

    update revengCAN

--- reverse_engineering_CAN.py
+++ reverse_engineering_CAN.py
@@ -1,4 +1,5 @@
 import math
+'''
 w_count = 0
 W = 50
 k = 0.5
@@ -34,3 +35,66 @@ with open("1min_DoStraffic.log") as f:
 			canid_list = []
 			id_i = 0
 		#print(log_split[2], end="")
+'''
+
+def 
+
+def PreProcessing(messageList, DLC):
+	payloadLen = len(messageList)
+	bitFlip = [0 for i in range(DLC)]
+	magnitude = [0 for i in range(DLC)]
+	previous = messageList[0]
+
+	while item in messageList:
+		for ix in range(0, DLC):
+			if item[ix] != previous[ix]:
+				bitFlip[ix] += 1
+
+	for ix in range(0, DLC):
+		bitFlip[ix] = bitFlip[ix]/payloadLen
+		magnitude = math.log10(bitFlip[ix])
+
+	return bitFlip,magnitude
+
+def Phase1(magnitude, DLC):
+	ref = list()
+	prevMagnitude = magnitude[0]
+	ixS = 0
+	for ix in range(0, DLC):
+		if magnitude[ix] < prevMagnitude :
+			ref.add((ixS, ix-1))
+			ixS = ix
+		prevMagnitude = magnitude[ix]
+	ref.add((ixS, DLC-1))
+	return ref
+
+def Phase2(ref, bitFlip):
+	rRef = list()
+	for sign in ref :
+		ixS = sign
+		ixE = sign
+		mu = mean(bitFlip[ixS:ixE])
+		std = stdDev(bitFlip[ixS:ixE])
+		if bitFlip[ixE] = 0 and matchCounter(bitFlip[ixS:ixE]):
+			rRef.add((ixS, ixE, COUNTER))
+		else if all(bitFlip[ixS:ixE]=0) and 0.5-std <= mu <= 0.5+std:
+			rRef.add((ixS, ixE, CRC))
+		else :
+			rRef.add((ixS, ixE, PHYS))
+	return rRef
+
+
+if __name__=='__main__':
+
+	for 
+		PreProcessing()
+
+
+
+
+
+
+
+
+
+
